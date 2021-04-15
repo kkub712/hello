234045de73e5bd5ba25b7c85aa9ecec44a5e33b0
kkub712 <82562303+kkub712@users.noreply.github.com>
2021-04-15 13:55:47 +0900

    MakeCodeプロジェクトの初期ファイル

---
+++ main.ts
@@ -0,0 +1,20 @@
+basic.forever(function () {
+    basic.showIcon(IconNames.StickFigure)
+    basic.pause(100)
+    basic.showLeds(`
+        . . # . .
+        # # # # #
+        # . # . #
+        # # # # #
+        . . # . .
+        `)
+    basic.pause(100)
+    basic.showLeds(`
+        . . # . .
+        . . # . .
+        # . # . #
+        # . # . #
+        . # # . .
+        `)
+    basic.pause(100)
+})
